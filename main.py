5e063cb38dc86ceff239339de55a7e80b1be79cc
karol <karol.krawczyk1@edu.uekat.pl>
2023-06-13 14:16:07 +0200

    create simple api to upload images

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/cifar-classifier/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
+
+
 if __name__ == '__main__':
-    pass
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
