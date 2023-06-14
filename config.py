0167796acea40284d40839e89bc999ec779e1e4f
karol <karol.krawczyk1@edu.uekat.pl>
2023-06-14 12:10:01 +0200

    add cifar10 classes dict for class prediction

--- config.py
+++ config.py
@@ -4,6 +4,18 @@ from keras.losses import CategoricalCrossentropy
 from keras.callbacks import EarlyStopping, LearningRateScheduler
 from keras.preprocessing.image import ImageDataGenerator
 
+CLASSES = {
+    0: 'airplane',
+    1: 'automobile',
+    2: 'bird',
+    3: 'cat',
+    4: 'deer',
+    5: 'dog',
+    6: 'frog',
+    7: 'horse',
+    8: 'ship',
+    9: 'truck'
+}
 BATCH_SIZE = 256
 EPOCHS = 120
 WARMUP_EPOCHS = 20
