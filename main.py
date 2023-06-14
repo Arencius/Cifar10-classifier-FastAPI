6a4fc1b7edab507c8a90571674cbfca3e462e709
karol <karol.krawczyk1@edu.uekat.pl>
2023-06-14 13:37:58 +0200

    implement function for loading images and predicting the class

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI, File, UploadFile
+import io
+import numpy as np
+from fastapi import FastAPI, File
 import uvicorn
+import PIL
+import keras
+import utils
+import config
 
 app = FastAPI()
+model = keras.models.load_model('src/classifier/model.h5')
 
 
-@app.post("/cifar-classifier/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+@app.post("/predict/")
+async def create_upload_file(image: bytes = File()):
+    image = PIL.Image.open(io.BytesIO(image))
+    image = utils.preprocess_image(image)
+
+    model_pred = model.predict(image)
+    predicted_class = np.argmax(model_pred)
+    prediction = config.CLASSES[predicted_class]
+
+    return {"Predicted class: ": prediction}
 
 
 if __name__ == '__main__':
